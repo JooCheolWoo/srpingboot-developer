4635b9211c9b29353890b3846fea76ef537f6fe6
Pobilover <tkfkdal@naver.com>
2023-06-26 23:40:31 +0900

    000

---
+++ src/main/java/com/galaxypoby/springbootdeveloper/service/UserService.java
@@ -0,0 +1,28 @@
+package com.galaxypoby.springbootdeveloper.service;
+
+import com.galaxypoby.springbootdeveloper.domain.User;
+import com.galaxypoby.springbootdeveloper.dto.AddUserRequest;
+import com.galaxypoby.springbootdeveloper.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Long save(AddUserRequest dto) {
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+    }
+
+    public User findById(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("Unexpected user"));
+    }
+}
